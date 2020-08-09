3da5688ecfd43da34d7411b8a89e90ef1beaa883
alishakavene <akmunywoki@gmail.com>
2020-08-09 20:25:05 +0300

    add migrations tags,user

--- insta/models.py
+++ insta/models.py
@@ -5,3 +5,16 @@ class User(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    User = models.ForeignKey(User,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import User,Post,tags
 
 # Create your tests here.
+class UserTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.john= User(first_name = 'John', last_name ='Doe', email ='john@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.john,User)) 
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_user(self):
+        self.save()
