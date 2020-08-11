1c416bd1cfff960300ddf6d3a0be7f31fb763e4a
alishakavene <akmunywoki@gmail.com>
2020-08-11 08:03:53 +0300

    add registration form

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
 from .models import Post
 from .forms import NewsLetterForm
@@ -7,20 +7,21 @@ from .email import send_welcome_email
 
 # Create your views here.
 
+
 def posts_today(request):
-   if request.method == 'POST':
+    posts = Post.todays_posts()
+    if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['your_name']
             email = form.cleaned_data['email']
-            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient = NewsLetterRecipients(name=name, email=email)
             recipient.save()
-            send_welcome_email(name,email)
-            
+            send_welcome_email(name, email)
             HttpResponseRedirect('posts_today')
-    else:
-        form = NewsLetterForm()
-    return render(request, 'insta-posts/today-posts.html', {"date": date,"posts":posts,"letterForm":form})
+        else:
+            form = NewsLetterForm()
+        return render(request, 'insta-posts/today-posts.html', {"date": date, "posts": posts, "letterForm": form})
 
 
 def past_days_posts(request, past_date):
@@ -36,7 +37,7 @@ def past_days_posts(request, past_date):
         return redirect(posts_today)
 
     posts = Post.days_posts(date)
-    return render(request, 'insta-posts/past-posts.html',{"date": date,"posts":posts})
+    return render(request, 'insta-posts/past-posts.html', {"date": date, "posts": posts})
 
 
 def search_results(request):
@@ -46,15 +47,16 @@ def search_results(request):
         searched_postss = Post.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'insta-posts/search.html',{"message":message,"postss": searched_postss})
+        return render(request, 'insta-posts/search.html', {"message": message, "postss": searched_postss})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'insta-postss/search.html',{"message":message})
+        return render(request, 'insta-postss/search.html', {"message": message})
 
-def post(request,post_id):
+
+def post(request, post_id):
     try:
-        post = Post.objects.get(id = post_id)
+        post = Post.objects.get(id=post_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"insta-posts/post.html", {"post":post})
+    return render(request, "insta-posts/post.html", {"post": post})
