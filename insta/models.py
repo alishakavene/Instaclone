a1ea41bd5a7682565440694fb415a6014f74fa65
alishakavene <akmunywoki@gmail.com>
2020-08-12 00:48:36 +0300

    make new migration file

--- insta/models.py
+++ insta/models.py
@@ -5,6 +5,19 @@ from django.contrib.auth.models import User
 # create models here
 
 
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
